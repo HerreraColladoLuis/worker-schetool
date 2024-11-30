6c6d95b45ac10ba2526723f37b4c00c818100275
Luis Herrera Collado <herreracolladoluis@gmail.com>
2024-11-30 21:04:52 +0100

    Add files via upload

--- schetool-app/employees_repository.py
+++ schetool-app/employees_repository.py
@@ -1,6 +1,15 @@
-import sqlite3
-from config import settings
-from dtos import employees_dto
+from dtos.employees_dto import EmployeesDto
+from utils.settings import DB_NAME
+from utils.database_utils import get_connection, ADD_EMPLOYEES_QUERY
 
 
-
+def add_employee(employee: EmployeesDto):
+    db = get_connection(DB_NAME)
+    cursor = db.cursor()
+    cursor.execute(ADD_EMPLOYEES_QUERY, (EmployeesDto.employee_name, EmployeesDto.employee_surname,
+                                         EmployeesDto.employee_email, EmployeesDto.employee_phone_number,
+                                         EmployeesDto.employee_weekly_hours, EmployeesDto.employee_role))
+    db.commit()
+    id_inserted = cursor.lastrowid
+    db.close()
+    return id_inserted
